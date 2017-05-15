fa41e795e0afb65e1dec1c5b7a6633f8dfbfb7ec
Idrees Ibraheem <idreesibraheem@gmail.com>
2017-05-15 17:14:50 +0100

    still in development

---
+++ public/javascript/app.js
@@ -0,0 +1,18 @@
+
+    var button = document.getElementById('btn');
+
+    button.onclick = addNew;
+
+
+
+    function addNew() {
+        var username = document.getElementById('username').value;
+
+        var newname = document.getElementById('newname').value;
+        console.log(username);
+        console.log(newname);
+
+    }
+
+
+
