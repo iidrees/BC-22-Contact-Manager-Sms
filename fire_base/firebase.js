a9f6cf8d2b68b138e1ca797965870fd9adf39493
Idrees Ibraheem <idreesibraheem@gmail.com>
2017-05-18 19:50:56 +0100

    updates

--- fire_base/firebase.js
+++ fire_base/firebase.js
@@ -3,12 +3,12 @@ var firebase = require("firebase");
 
   // Initialize Firebase
 var config = {
-apiKey: "AIzaSyDnyB5FfLDrCOtB3fhNdcVsyesz-GtIv7I",
-authDomain: "bc-22-contact-manager-sms.firebaseapp.com",
-databaseURL: "https://bc-22-contact-manager-sms.firebaseio.com",
-projectId: "bc-22-contact-manager-sms",
-storageBucket: "bc-22-contact-manager-sms.appspot.com",
-messagingSenderId: "974884069082"
+apiKey: process.env.apiKey,
+authDomain: process.env.authDomain,
+databaseURL: process.env.databaseURL,
+projectId: process.env.projectId,
+storageBucket: process.env.storageBucket,
+messagingSenderId: process.env.messagingSenderId
 };
 module.exports = firebase.initializeApp(config);
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // require modules
 var express = require('express');
 var fs = require('fs');
+var dotenv = require('dotenv').config()
 var userSignin = require('./controller/contact.js'); 
 var addContact = require('./controller/add.js');
 var importContact = require('./controller/importcontacts.js');
@@ -14,77 +15,23 @@ var addRef = contactRef.child('import_contact');
 var fireAuth = firebase.auth();
 
  
-// 
+// Start our express application
 var app = express();
 
 // set the port of our application
-
 //process.env.PORT lets the port be set by heroku
-
-var port = process.env.PORT || 9090;
+var port = process.env.PORT;
 
 //set up the template engine
 app.set('view engine', 'ejs');
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
-//Fire controllers
-
+// run controllers
 userSignin(app);
 addContact(app);
 importContact(app);
 
-
-
-
-/*function getData(data) {
-	//console.log(data.val());
-	var numbers = data.val();
-	var keys = Object.keys(numbers);
-	//console.log(keys);
-	for (var i = 0; i < keys.length; i++) {
-		var k = keys[i];
-		var name = numbers[k].name;
-		var number = numbers[k].number;
-	    //console.log(name, number); 
-	}
-	return (name, number); 
-}
-function errData(err) {
-	console.log('Error!');
-	console.log(err);
-}
-
-//var details = getData();
-app.get('/home', urlencodedParser, function(req, res) {
-	var detail;
-	addRef.on('value', function(data){
-		 detail = data.val();
-		 var keys = Object.keys(detail);
-		//console.log(detail)
-		console.log(keys);
-		var newName = [];
-		var newNumber = []; 
-		for (var i = 0; i < keys.length; i++) {
-			var k = keys[i];
-			var name = detail[k].name;
-			var number = detail[k].number;
-		    //console.log(name, number);
-		    newNumber.push(name,number);
-		    //newName.push(name);
-	}
-	console.log(newName, newNumber);
-		//var name = detail.
-		res.render('home', {detail: newNumber});
-	})
-    
-	res.render('home', {detail: detail});
-
-
-
-})*/
-
-// testing my middleware
-
+// routers
 app.get('/', function(req, res) {
 	res.render('register');
 })
@@ -92,11 +39,6 @@ app.get('/', function(req, res) {
 app.get('/register', function(req, res) {
 	res.render('register');
 })
-
-
-
-
-
 
 
 app.get('/home', function(req, res) {
