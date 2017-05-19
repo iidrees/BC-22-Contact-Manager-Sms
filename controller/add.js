1ad6cdea88476f3ae6bf8a1ddfebc59bcecbd90a
Idrees Ibraheem <idreesibraheem@gmail.com>
2017-05-19 08:08:06 +0100

    add comment

--- controller/add.js
+++ controller/add.js
@@ -3,16 +3,10 @@ var bodyParser = require('body-parser');
 
 
 var firebase = require('../fire_base/firebase.js');
-//var db = firebase.database();
-//var ref = db.ref('contacts');
-//var logRef = ref.child('logs');
-//var fireAuth = firebase.auth();
-//var contactsRef = ref.child('contact details');
 
 //Connect to the database
 var db = firebase.database();
 var ref = db.ref('contacts');
-//var addRef = ref.child('logs');
 var fireAuth = firebase.auth();
 
 
@@ -31,27 +25,10 @@ app.post('/addcontacts', urlencodedParser, function(req, res) {
         email: email, 
         number: number, 
         timeStamp: new Date().toString()};
-    //console.log(contacts);
-
-    //var user = fireAuth.currentUser;
-/*  if (user) {
-        userId = user.uid;
-
-    //addRef.child('/' + userId).push(contacts).then(res.redirect('/addcontacts'))
-         .catch(function(err) {
-                var errorCode = err.code;
-                var errorMessage = err.message;
-                return res.render('/register', {error: errorMessage});
-            });
-
-
-    }*/
-
     
     var contactsRef = ref.child('contact details');
     var contactRef = contactsRef.push(contacts);
 
-    //addRef.child(contactRef.key).set(contacts);
     contactsRef.orderByKey().limitToLast(1).on('child_added', function(snap) {
         console.log('added', snap.val());
     });
